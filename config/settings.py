88899e21312c1a9f59092f868a67fc156bc66b6f
johannaPrieto <johanna.esquivel@uabc.edu.mx>
2026-05-10 10:41:48 +0000

    Agrega ALLOWED_HOSTS para Render

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-*xl3%=z&#8a@$&)i8=+jcp=o3he6esls)bog+@a*4p!=vv$+hw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mi-proyecto-django-0bd6.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
